4dd6aa81a9dc28c9854103b4ac1db3f077c818e8
Marc <marc@fintory.com>
2020-06-23 14:14:00 +0200

    edit Footer

--- components/footerComponent/styles.js
+++ components/footerComponent/styles.js
@@ -2,20 +2,24 @@ import styled from 'styled-components'
 
 export const FooterWrapper = styled.div`
   display: flex;
-  width: 100%;
-  height: 7.5rem;
   flex-direction: row;
+  flex-wrap: wrap;
   justify-content: center;
   align-items: center;
+  width: 100%;
+  height: 7.5rem;
   background-color: ${props => props.theme.root.mainBackgroundColor};
   color: ${ props => props.theme.fontColor.white };
 
   @media (max-width: 575.98px) {
     height: 3.5rem;
+    padding: 0;
+    margin-top: .75rem;
   }
 
   @media (min-width: 576px) {
     height: 3.5rem;
+    margin-top: .75rem;
   }
 
   @media (min-width: 768px) {
@@ -28,17 +32,22 @@ export const FooterWrapper = styled.div`
 `
 
 export const FooterLinks = styled.a `
- color: #b0b0b0;
- font-size: ${ props => props.theme.fontSize.fsSmall0 };
- padding: 0 8px;
- text-decoration: underline;
+  width: 100%;
+  color: #b0b0b0;
+  font-size: ${ props => props.theme.fontSize.fsSmall0 };
+  padding: 0 8px;
+  text-decoration: underline;
 
  @media (max-width: 575.98px) {
     font-size: ${props => props.theme.fontSize.fsSmall0};
+    margin-top: .25rem;
+    padding: 0;
   }
 
   @media (min-width: 576px) {
     font-size: ${ props => props.theme.fontSize.fsSmall00};
+    margin-top: .25rem;
+    padding: 0;
   }
 
   @media (min-width: 768px) {

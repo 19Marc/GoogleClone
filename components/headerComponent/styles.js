61cf61228344332fa6b4342145a958e2a426c200
Marc <marc@fintory.com>
2020-06-23 14:01:53 +0200

    edit Header Headlin

--- components/headerComponent/styles.js
+++ components/headerComponent/styles.js
@@ -10,23 +10,23 @@ export const HeaderWrapper = styled.div`
   background-color: ${ props => props.theme.root.MainBackgroundColor };
   color: ${ props => props.theme.fontColor.white };
 
-    @media (max-width: 575.98px) {
-          height: 6rem;
-          font-size: ${ props => props.theme.fontSize.fsSmall1 };
-          line-height: ${ props => props.theme.lineHeight.lhSmall0};
-      }
-
-    @media (min-width: 576px) {
-        height: 6rem;
+  @media (max-width: 575.98px) {
+    height: 6rem;
+    font-size: ${ props => props.theme.fontSize.fsSmall1 };
+    line-height: ${ props => props.theme.lineHeight.lhSmall0};
     }
 
-    @media (min-width: 768px) {
-        height: 6rem;
-    }
+  @media (min-width: 576px) {
+    height: 6rem;
+  }
 
-    @media (min-width: 1200px) {
-      height: 13rem;
-    }
+  @media (min-width: 768px) {
+      height: 6rem;
+  }
+
+  @media (min-width: 1200px) {
+    height: 13rem;
+  }
 `
 
 export const Headline = styled.h1` 
@@ -45,6 +45,7 @@ export const Headline = styled.h1`
     align-items: center;
     height: 1.5rem;
     font-size: ${ props => props.theme.fontSize.fsMiddle0 };
+    margin: 0;
   }
 
   @media (min-width: 576px) {
@@ -54,6 +55,7 @@ export const Headline = styled.h1`
     align-items: center;
     height: 1.5rem;
     font-size: ${ props => props.theme.fontSize.fsMiddle0 };
+    margin: 0;
   }
 
   @media (min-width: 768px) {
@@ -74,11 +76,11 @@ export const Headline = styled.h1`
       color: ${ props => props.theme.fontColor.white};
       
         @media (max-width: 575.98px) {
-          margin-right: .1;
+          margin-right: 0;
         }
 
         @media (min-width: 576px) {
-          margin-right: .2rem;
+          margin-right: 0rem;
         }
 
         @media (min-width: 768px) {
@@ -140,28 +142,4 @@ export const Description = styled.p`
       margin: 1rem 0 0rem 0;
     }
 `
-
-// const HeaderContainer = ({ children, className}) => (
-//   <div>
-//     {Title}
-//   </div>
-// )
-
-// export default styled(HeaderContainer)`
-//   width: 100%;
-//   height: 150px;
-//   display: flex;
-//   justify-content: center;
-//   align-items: center;
-//   background-color: purple;
-//   color: white;
-// `
-
-// export const Title = styled.title`
-//   display: block;
-//   text-align: left;
-//   width: 100%;
-//   color: black;
-//   font-weight: bold;
-// `;
 

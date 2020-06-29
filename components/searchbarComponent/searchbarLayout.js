f18dc661398576202a996b7309bac62e995bef6c
Marc <marc@fintory.com>
2020-06-29 09:19:33 +0200

    add axios and making changes in searchbarlayout.js

--- components/searchbarComponent/searchbarLayout.js
+++ components/searchbarComponent/searchbarLayout.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 // import { getServerSideProps } from 'next'
 
 import { SearchbarWrapper, SearchbarInput, SearchWrapper, InputWrapper, SearchButton, TextWrapper, GoogleImage, SafedHours } from './styles'
@@ -25,58 +26,77 @@ import { SearchbarWrapper, SearchbarInput, SearchWrapper, InputWrapper, SearchBu
   //   setFormData({ ...formValue, [event.target.name]: event.target.value });
   // };
 
+
+
+  // useEffect(() => {
+  //   async function  fetchRepos() {
+  //     const  response  = await fetch(`https://api.github.com/users/${userName}/repos`)
+  //     const  repos  = await response.json()
+
+  //     // our setRepos call tells React to re-render the component.
+  //     // which then calls our useEffect hook again, so on and so forth
+  //     setRepos()
+  //   } 
+  //   fetchRepos().catch(error  =>  console.error(error))
+  //   // this is because we are not passing an array of 
+  //   // dependencies as the second argument to useEffect
+  // })
+
+  // const handleSubmit = (e) => {
+  //   e.preventDefault()
+  //   setUserName(e.target.username.value)
+  // };
+
 export default function SearchbarComponent() {
 
   const [formData, setFormData] = useState ('');
 
-  const [response, setResponse] = useState({
-    type: '',
-    message: ''
-  });
+  const [data, setData] = useState({ items: [] });
+
+  // const [response, setResponse] = useState({
+  //   type: '',
+  //   message: '',
+  // });
+
+  useEffect( () => {
+    const fetchData = async () => {
+      const api = await axios(`https://www.googleapis.com/customsearch/v1?key=AIzaSyAaLsnvAyqBWIVjMBOzw0DEQADgvPa0xwc&cx=003843309577069876842:stoc1spwaxe&q=${formData}`);
+      // const getData = await api.json();
+      console.log("apiData", api.data)
+      // console.log("2getData", getData.items.cacheId)
+      // const safeData = await getData.items.map(data => data.title);
+      // console.log("safeData: ", safeData)
+      // console.log("get DATA", getData.items)
+      // console.log("map", getData.items.map(data => data.cacheId))
+     
+      setData(api.data);
+    };
+    fetchData();
+  }, []);
+
+  // console.log("data", data)
+
+  // async function getData() {
+  //   const api = await fetch(`https://www.googleapis.com/customsearch/v1?key=AIzaSyAaLsnvAyqBWIVjMBOzw0DEQADgvPa0xwc&cx=003843309577069876842:stoc1spwaxe&q=${formData}`);
+  //   const result = await fetch(api);
+  //   const getResult = await result.json();
+  //   setFormData(getResult);
+  //   console.log(getResult)
+  //   }
 
   const handleSubmit = async event => {
     event.preventDefault()
-    console.log(formData)
-    // query(formData);
-
-    try {
-      const res = await fetch(`https://www.googleapis.com/customsearch/v1?key=AIzaSyAaLsnvAyqBWIVjMBOzw0DEQADgvPa0xwc&cx=003843309577069876842:stoc1spwaxe&q=${formData}`, {
-        method: 'GET',
-        headers: { 'Content-Type': 'application/json' }
-      });
-      console.log(res)
-      const json = await res.json();
-      const searchResult = await json.items
-      console.log("jsonitems", json.items)
-      console.log("searchResults", searchResult)
-      j
-
-      if (json.success) {
-        setResponse({
-          type: 'success',
-          message: 'Thank you for reaching out to us.'
-        });
-      } else {
-        setResponse({
-          type: 'error',
-          message: json.message
-        });
-      }
-    } catch (e) {
-      console.log('An error occurred', e);
-      setResponse({
-        type: 'error',
-        message: 'An error occured while submitting the form'
-      });
-    }
-  };
-
-  // const [keyData, setKeyData] = useState ({
-  //   ApiKey: "AIzaSyAaLsnvAyqBWIVjMBOzw0DEQADgvPa0xwc&",
-  //   GoogleKey: "003843309577069876842:stoc1spwaxe&q",
-  //   query: null,
-  // });
+    // console.log(event.target.name)
+    // console.log(event.target.value)
+    // .then(response => response.json())
+    // .then
+    // console.log(formData)
   
+
+
+  }
+
+
   return (
     <SearchbarWrapper>
       <SearchWrapper>
@@ -89,80 +109,19 @@ export default function SearchbarComponent() {
         </InputWrapper>
         <SearchButton>Search</SearchButton>
       </SearchWrapper>
+      <ul>
+        <li color="white"></li>
+        <li>1</li>
+        <li>1</li>
+        <li>1</li>
+      </ul>
+      {/* <ul>
+        {data.items.map(item => (
+          <li key={item.cacheId}>
+            <p>{item.title}</p>
+          </li>
+        ))}
+      </ul> */}
     </SearchbarWrapper>
   )
-}
-// export async function getServerSideProps(context) {
-//   const res = await fetch(
-//       `https://www.googleapis.com/customsearch/v1?key=` + {ApiKey} + `cx=` + {GoogleKey} + `=` + {query}
-//   ).then((res) => res.json());
-//   console.log("getServerProps", res, context.params, context.query);
-//   return {
-//       props: { ...res, name: context.query.slug }
-//   }
-// };
-
-
-// export async function getServerSideProps(context) {
-//   const res = await fetch(
-//       "https://www.googleapis.com/customsearch/v1?key=AIzaSyAaLsnvAyqBWIVjMBOzw0DEQADgvPa0xwc&cx=003843309577069876842:stoc1spwaxe&q=google"
-//   ).then((res) => res.json());
-//   console.log("getServerProps", res, context.params, context.query);
-//   return {
-//       props: { ...res, name: context.query.slug }
-//   }
-// };
-
-
-
-// import { useState } from 'react';
-
-// import { SearchbarWrapper, SearchbarInput, SearchWrapper, InputWrapper, SearchButton, TextWrapper, GoogleImage, SafedHours } from './styles'
-// import { getServerSideProps } from 'next'
-
-
-// const ApiKey = "AIzaSyAaLsnvAyqBWIVjMBOzw0DEQADgvPa0xwc&";
-// const GoogleKey = "003843309577069876842:stoc1spwaxe&q";
-// query = "";
-
-// handleSubmit = (event) => {
-//   event.preventDefault()
-// }
-
-// handleOnChange = (event) => {
-//   event.preventDefault()
-//   console.log(event)
-//   console.log(event.target.name)
-//   console.log(event.target.value)
-// }
-
-// export default function SearchbarComponent() {
-
-  // const [formValue, setFormData] = useState ({
-  //   name: "",
-  // });
-
-  // const [keyData, setKeyData] = useState ({
-  //   ApiKey: "AIzaSyAaLsnvAyqBWIVjMBOzw0DEQADgvPa0xwc&",
-  //   GoogleKey: "003843309577069876842:stoc1spwaxe&q",
-  //   query: null,
-  // });
-
-  // const handleSubmit = (event) => {
-  //   event.preventDefault()
-  //   console.log(formValue)
-  // };
-  
-  // const handleOnChange = (event) => {
-  //   event.preventDefault()
-  //   setFormData({ ...formValue, [event.target.name]: event.target.value });
-  // };
-  // return (
-  //   <SearchbarWrapper>
-  //     <SearchWrapper>
-  //       <InputWrapper id="form" onclick={handleSubmit}>
-  //         <SearchbarInput id="search" type="text" placeholder="Type your search" name="searchName" onChange={handleOnChange}/>
-  //         <TextWrapper>
-  //           <GoogleImage>Google</GoogleImage>
-  //           <SafedHours>xxx Watt hours safed</SafedHours>
-  //         </TextWrapper>
+};
